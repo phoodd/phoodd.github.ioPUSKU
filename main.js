ec35cea553c4103c687e2e367910579a6f1eb331
sten <poisonpeer@gmail.com>
2022-11-04 17:36:36 +0200

    navbar color change and background pics

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const images =["pic/pic1.jpg","pic/pic2.jpg","pic/pic3.jpg"];
+const carousel = document.querySelector(".carousel");
+const interval = setInterval(function() {
+    startCarousel();
+}, 10000);
+var index = 1;
+
+startCarousel = () => {
+    carousel.style.backgroundImage = `url(${images[index++]})`;
+    carousel.classList.remove("zoom");
+    void carousel.offsetWidth;
+    carousel.classList.add("zoom");
+    if(index > images.length - 1) index = 0;
+  }
